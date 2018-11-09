8f64991beaf145fe5c4cd3f54a56cc15572271fc
Will Crichton <wcrichto@cs.stanford.edu>
2018-11-08 17:11:42 -0800

    Move TCP tests into tcp.rs, more instructions on counterex/tcp modules

--- assign7/program/src/lib.rs
+++ assign7/program/src/lib.rs
@@ -1,62 +1,11 @@
 extern crate rand;
 
-pub mod session_bug;
-//mod counterex;
-pub mod session;
+// UNCOMMENT this when doing section 1, and RE-COMMENT it when doing section 2
+mod counterex;
+
+// UNCOMMENT this when doing section 2
 pub mod tcp;
+
+pub mod session_bug;
+pub mod session;
 pub mod atm;
-
-#[cfg(test)]
-mod test {
-  use session::*;
-  use session::NOISY;
-  use std::sync::atomic::Ordering;
-  use rand;
-  use rand::Rng;
-  use tcp::*;
-  use std::marker::PhantomData;
-  use std::sync::mpsc::channel;
-  use std::thread;
-
-  fn gen_bufs() -> Vec<Buffer> {
-    let mut bufs: Vec<Buffer> = Vec::new();
-    let mut rng = rand::thread_rng();
-    for _ in 0usize..20 {
-      let buf: Buffer = vec![0; rng.gen_range(1, 10)];
-      let buf: Buffer = buf.into_iter().map(|x: u8| rng.gen()).collect();
-      bufs.push(buf);
-    }
-    bufs
-  }
-
-  #[test]
-  fn test_basic() {
-    let bufs = gen_bufs();
-    let bufs_copy = bufs.clone();
-    let (s, c): ((Chan<(), TCPServer>), (Chan<(), TCPClient>)) = Chan::new();
-    let thread = thread::spawn(move || { tcp_client(c, bufs); });
-
-    let recvd = tcp_server(s);
-    let res = thread.join();
-
-    assert_eq!(recvd, bufs_copy);
-  }
-
-  #[test]
-  fn test_lossy() {
-    let bufs = gen_bufs();
-    let bufs_copy = bufs.clone();
-
-    NOISY.with(|noisy| {
-      noisy.store(true, Ordering::SeqCst);
-    });
-
-    let (s, c): ((Chan<(), TCPServer>), (Chan<(), TCPClient>)) = Chan::new();
-    let thread = thread::spawn(move || { tcp_client(c, bufs); });
-
-    let recvd = tcp_server(s);
-    let res = thread.join();
-
-    assert_eq!(recvd, bufs_copy);
-  }
-}
--- assign7/program/src/tcp.rs
+++ assign7/program/src/tcp.rs
@@ -25,3 +25,56 @@ pub fn tcp_server(c: Chan<(), TCPServer>) -> Vec<Buffer> {
 pub fn tcp_client(c: Chan<(), TCPClient>, bufs: Vec<Buffer>) {
   unimplemented!();
 }
+
+#[cfg(test)]
+mod test {
+  use session::*;
+  use session::NOISY;
+  use std::sync::atomic::Ordering;
+  use rand;
+  use rand::Rng;
+  use tcp::*;
+  use std::marker::PhantomData;
+  use std::sync::mpsc::channel;
+  use std::thread;
+
+  #[test]
+  fn test_basic() {
+    let mut bufs: Vec<Buffer> = Vec::new();
+    let mut rng = rand::thread_rng();
+    for i in 0usize..20 {
+      let buf: Buffer = vec![0; rng.gen_range(1, 10)];
+      let buf: Buffer = buf.into_iter().map(|x: u8| rng.gen()).collect();
+      bufs.push(buf);
+    }
+
+    let (s, c): ((Chan<(), TCPServer>), (Chan<(), TCPClient>)) = Chan::new();
+    let thread = thread::spawn(move || { tcp_client(c, bufs); });
+
+    tcp_server(s);
+
+    let res = thread.join();
+  }
+
+  #[test]
+  fn test_noisy() {
+    let mut bufs: Vec<Buffer> = Vec::new();
+    let mut rng = rand::thread_rng();
+    for i in 0usize..20 {
+      let buf: Buffer = vec![0; rng.gen_range(1, 10)];
+      let buf: Buffer = buf.into_iter().map(|x: u8| rng.gen()).collect();
+      bufs.push(buf);
+    }
+
+    NOISY.with(|noisy| {
+      noisy.store(true, Ordering::SeqCst);
+    });
+
+    let (s, c): ((Chan<(), TCPServer>), (Chan<(), TCPClient>)) = Chan::new();
+    let thread = thread::spawn(move || { tcp_client(c, bufs); });
+
+    tcp_server(s);
+
+    let res = thread.join();
+  }
+}
